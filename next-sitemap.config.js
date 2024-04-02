00e3a4791d3e21f4ee037f6d4428f46e42cbc88c
devmithun89 <mithungorai662@gmail.com>
2024-04-02 10:54:33 -0700

    added site map

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://erex.in",
+  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
+  outDir: "./out",
+};

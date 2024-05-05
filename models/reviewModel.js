d77216dd4ba626463e6c8ef4eb4755f05c60ef0f
nayan-dey <nayande.work@gmail.com>
2024-05-05 16:41:44 +0530

    cured review route

---
+++ models/reviewModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const ReviewsSchema = new mongoose.Schema({
+  client: {
+    type: String,
+    required: true,
+  },
+  review: {
+    type: String,
+    required: false,
+  },
+  rating: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.models.Reviews || mongoose.model("Reviews", ReviewsSchema);
---
+++ utils/index.js
@@ -0,0 +1,2 @@
+export const DB_URL =
+  "mongodb+srv://Erex-Studio:JwpyVBrL5sx7iNC5@erex-studio-portfollio.zinrvcg.mongodb.net/portfollio";
